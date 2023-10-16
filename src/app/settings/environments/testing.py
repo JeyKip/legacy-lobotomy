41ee70d4d579d76f6bf4fb38445c8fa49a1fd414
Yevhen Nosolenko <jeykip1990@gmail.com>
2023-10-17 02:07:53 +0300

    Configured running tests with PyTest

--- src/app/settings/environments/testing.py
+++ src/app/settings/environments/testing.py
@@ -1,2 +1,7 @@
+import os
+from ..sources.env import read_env
+
+read_env(f'{os.path.dirname(os.path.realpath(__name__))}/.env.testing')
+
 from ..configs.base import *
 from ..configs.media import *
