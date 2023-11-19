29d3c1488bb26f11ca76ba72631b61154931f885
Yevhen Nosolenko <jeykip1990@gmail.com>
2023-11-20 01:32:40 +0200

    Added auth_client and auth_client_factory fixtures; implemented tests to demonstrate vulnerabilities in the existing profile management functionality

--- src/conftest.py
+++ src/conftest.py
@@ -5,3 +5,21 @@ import pytest
 def client():
     from rest_framework.test import APIClient
     return APIClient()
+
+
+@pytest.fixture
+def auth_client(user, auth_client_factory):
+    return auth_client_factory(user)
+
+
+@pytest.fixture
+def auth_client_factory():
+    def auth_client(user):
+        from rest_framework.authtoken.models import Token
+        from rest_framework.test import APIClient
+
+        Token.objects.get_or_create(user=user)
+
+        return APIClient(HTTP_AUTHORIZATION=f'Token {user.auth_token.key}')
+
+    return auth_client
