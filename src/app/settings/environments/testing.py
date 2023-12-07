f959d608837541d18128659299f7f4af180e205e
Yevhen Nosolenko <jeykip1990@gmail.com>
2023-12-08 00:52:24 +0200

    Overridden default format for sending test requests from "multipart" to "json"

--- src/app/settings/environments/testing.py
+++ src/app/settings/environments/testing.py
@@ -5,3 +5,8 @@ read_env(f'{os.path.dirname(os.path.realpath(__name__))}/.env.testing')
 
 from ..configs.base import *
 from ..configs.media import *
+
+REST_FRAMEWORK = {
+    **REST_FRAMEWORK,
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
+}
