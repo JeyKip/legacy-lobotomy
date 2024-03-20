725e9c2344e29055d6334f7c648e98d71b006bb6
Yevhen Nosolenko <jeykip1990@gmail.com>
2024-03-20 11:32:13 +0200

    Updated test environment configuration and example test module

--- src/users/tests/api/test_password_reset.py
+++ src/users/tests/api/test_password_reset.py
@@ -1,6 +1,51 @@
 import pytest
 
 
-@pytest.mark.django_db
-def test_admin_user_fixture(admin_user):
-    assert admin_user.is_superuser
+class TestPasswordReset:
+    @pytest.mark.django_db
+    def test_admin_user_fixture_1(self, admin_user):
+        assert admin_user.is_superuser
+
+    @pytest.mark.django_db
+    def test_admin_user_fixture_2(self, admin_user):
+        assert admin_user.is_superuser
+
+    @pytest.mark.django_db
+    def test_admin_user_fixture_3(self, admin_user):
+        assert admin_user.is_superuser
+
+    @pytest.mark.django_db
+    def test_admin_user_fixture_4(self, admin_user):
+        assert admin_user.is_superuser
+
+    @pytest.mark.django_db
+    def test_admin_user_fixture_5(self, admin_user):
+        assert admin_user.is_superuser
+
+    @pytest.mark.django_db
+    def test_admin_user_fixture_6(self, admin_user):
+        assert admin_user.is_superuser
+
+    @pytest.mark.django_db
+    def test_admin_user_fixture_7(self, admin_user):
+        assert admin_user.is_superuser
+
+    @pytest.mark.django_db
+    def test_admin_user_fixture_8(self, admin_user):
+        assert admin_user.is_superuser
+
+    @pytest.mark.django_db
+    def test_admin_user_fixture_9(self, admin_user):
+        assert admin_user.is_superuser
+
+    @pytest.mark.django_db
+    def test_admin_user_fixture_10(self, admin_user):
+        assert admin_user.is_superuser
+
+    @pytest.mark.django_db
+    def test_admin_user_fixture_11(self, admin_user):
+        assert admin_user.is_superuser
+
+    @pytest.mark.django_db
+    def test_admin_user_fixture_12(self, admin_user):
+        assert admin_user.is_superuser
